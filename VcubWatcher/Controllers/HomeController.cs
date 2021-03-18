59850e3b4f66c54bc739e05fe4770e75c3f48627
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-03-18 11:54:02 +0100

    que du sale

--- VcubWatcher/Controllers/HomeController.cs
+++ VcubWatcher/Controllers/HomeController.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using VcubWatcher.Models;
 
 namespace VcubWatcher.Controllers
@@ -26,6 +31,26 @@ namespace VcubWatcher.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        private static List<BikeStation> GetBikeStationFromAPI()
+        {
+            using (var client = new HttpClient())
+            {
+                // GET data from URL
+                var response = client.GetAsync("https://api.alexandredubois.com/vcub-backend/vcub.php");
+                // GET http result en string
+                var StringResult = response.Result.Content.ReadAsStringAsync();
+                // Conversion du JSON en collection d'objets
+                var res = JsonConvert.DeserializeObject<List<BikeStation>>(StringResult.Result);
+                return res;
+            }
+
+        }
+        public IActionResult StationList()
+        {
+            var stations = GetBikeStationFromAPI();
+            return View(stations);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
